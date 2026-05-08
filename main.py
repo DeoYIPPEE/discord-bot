fd184e092e81770c903017e12b52e8a321ab2142
Deo <deopvpzyt@gmail.com>
2026-05-08 17:20:40 +0530

    feat: add server status messages

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
 import dotenv
 import os
 import discord
-import logging
+from discord import app_commands
+from discord.ext import commands
+from discord.ext import tasks
+#import logging
 from mcstatus import JavaServer as mcs
 
 dotenv.load_dotenv()
 TOKEN = str(os.getenv("DISCORD_TOKEN"))
-
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+#handler = logging.FileHandler(filename="discord.log", mode="a", encoding="utf-8")
 
 intents = discord.Intents.all()
-bot = discord.Bot(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+server_up_cache = None
+embed_cycle = [discord.Embed(title="SERVER IS NOW ONLINE", color=discord.Color.green()), discord.Embed(title="SERVER IS OFFLINE", color=discord.Color.red())]
 
 @bot.event
 async def on_ready():
     print("Bot starting!")
 
-@bot.command(name="status", description="Gets status of SMP", integration_types={discord.IntegrationType.user_install},contexts={discord.InteractionContextType.private_channel, discord.InteractionContextType.guild})
-async def status(ctx):
+    try:
+        synced = await bot.tree.sync()
+        print("SMP bot synced!")
+    except Exception as e:
+        print(f"An exception occurred while syncing the bot: {e}")
+
+    try:
+        statusChangeMessage.start()
+    except Exception as e:
+        print(f"An exception occurred while starting a task: {e}")
+
+
+
+@tasks.loop(seconds = 10)
+async def statusChangeMessage():
+    try:
+        server = mcs.lookup("vocation-publicity.gl.joinmc.link:25565")
+        status = server.status()
+        server_up = True
+    except Exception:
+        server_up = False
+    global server_up_cache
+    global embed_cycle
+    if server_up_cache != server_up:
+        server_up_cache = server_up
+        channel = bot.get_channel(1502203891813716079)
+        if server_up:
+            embed = embed_cycle[0]
+        else:
+            embed = embed_cycle[1]
+        print(server_up)
+        await channel.send(embed=embed)
+    
+
+@bot.tree.command(name="status", description="Gets status of SMP") #, integration_types={bot.IntegrationType.user_install},contexts={bot.InteractionContextType.private_channel, discord.InteractionContextType.guild}
+@app_commands.allowed_contexts(dms=True, private_channels=True, guilds=True)
+async def status(ctx: discord.Interaction):
     try:
         server = mcs.lookup("vocation-publicity.gl.joinmc.link:25565")
         status = server.status()
 
-        await ctx.respond(f"The server is online with {status.players.online} players currently online")
+        await ctx.response.send_message(f"The server is online with {status.players.online} players currently online")
     except Exception:
-        await ctx.respond(f"The server is offline.")
+        await ctx.response.send_message(f"The server is offline.")
         user = await bot.fetch_user(1173455513288196127)
         try:
             await user.send('start server idiot')
